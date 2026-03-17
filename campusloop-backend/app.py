f6727e497a2b930fcc3f1c8a5faef05a498b5e09
Mahammad Mashood <mashoodrenja17@gmail.com>
2026-03-17 21:03:26 +0530

    Production ready - Procfile, gunicorn, eventlet

--- campusloop-backend/app.py
+++ campusloop-backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, app
+from flask import Flask
 from flask_cors import CORS
 from config import Config
 from extensions import db, jwt, socketio, migrate
@@ -11,7 +11,11 @@ def create_app():
     jwt.init_app(app)
     migrate.init_app(app, db)
     CORS(app, origins="*")
-    socketio.init_app(app, cors_allowed_origins="*")
+    socketio.init_app(
+        app,
+        cors_allowed_origins="*",
+        async_mode="eventlet"
+    )
 
     from models.user import User
     from models.post import Post
@@ -19,21 +23,20 @@ def create_app():
     from models.upvote import Upvote
     from models.bookmark import Bookmark
 
-    
     from routes.auth import auth_bp
     from routes.posts import posts_bp
+    from routes.notifications import notifications_bp
     from routes.users import users_bp
-    app.register_blueprint(users_bp, url_prefix="/api/users")
     app.register_blueprint(auth_bp, url_prefix="/api/auth")
     app.register_blueprint(posts_bp, url_prefix="/api/posts")
+    app.register_blueprint(notifications_bp, url_prefix="/api/notifications")
+    app.register_blueprint(users_bp, url_prefix="/api/users")
 
     from sockets.events import handle_connect, handle_join_branch, handle_disconnect
 
-    from routes.notifications import notifications_bp
-    app.register_blueprint(notifications_bp, url_prefix="/api/notifications")
-
     return app
 
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
-    socketio.run(app, debug=True, port=5000)
+    socketio.run(app, debug=False, port=5000)
